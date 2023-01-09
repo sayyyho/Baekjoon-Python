54ef5aea92dcb01e826eaa7dab22e62fa36bbed8
sayyyho <323psh@naver.com>
2023-01-09 19:11:05 +0900

    solved : tag - dp(1912)

--- dp/1912.py
+++ dp/1912.py
@@ -3,8 +3,7 @@ import sys
 n = int(input())
 dp = [0 for _ in range(n)]
 data = list(map(int, sys.stdin.readline().split()))
-dp[0] = data[0]
-my_max = dp[0]
+my_max = dp[0] = data[0]
 for i in range(1, n):
     dp[i] = max(dp[i-1] + data[i], data[i])
     if(my_max<dp[i]):my_max = dp[i]
