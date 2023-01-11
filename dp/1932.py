6b1c2adb743ec273f4cdb3a4b3e1ef36a917bbd7
sayyyho <323psh@naver.com>
2023-01-11 17:55:51 +0900

    solved : tag - dp(1932)

---
+++ dp/1932.py
@@ -0,0 +1,12 @@
+n = int(input())
+tri = []
+for _ in range(n):
+    tri.append(list(map(int, input().split())))
+
+for k in range(1, n):
+    tri[k][0] += tri[k-1][0]
+    tri[k][k] += tri[k-1][k-1]
+    for i in range(1,k):
+        tri[k][i] += max(tri[k-1][i-1], tri[k-1][i])
+print(max(tri[n-1]))
+
