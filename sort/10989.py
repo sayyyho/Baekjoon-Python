ba45fd2c837243658ea71a45176cf055fc68b784
sayyyho <323psh@naver.com>
2023-01-11 22:17:44 +0900

    solved : tag - sort(10989)

--- sort/10989.py
+++ sort/10989.py
@@ -1,13 +1,10 @@
 import sys
 
 n = int(input())
-count_arr = [0] * 10001
 
-for _ in range(n):
-    number = int(sys.stdin.readline())
-    count_arr[number] +=1
-
-for index in range(10001):
-    while(count_arr[index] > 0):
-        print(index)
-        count_arr[index] -= 1
+memos = [0] * 10001
+for _ in range(n):memos[int(sys.stdin.readline())] += 1
+for i in range(1, 10001):
+    while(memos[i]):
+        sys.stdout.write(str(i) + '\n')
+        memos[i] -=1
