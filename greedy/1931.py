c9199703bea063a36f29a2879044e663966184c1
sayyyho <323psh@naver.com>
2023-02-04 02:19:55 +0900

    solved : tag - greedy(1931)

--- greedy/1931.py
+++ greedy/1931.py
@@ -1,3 +1,12 @@
 n = int(input())
-conf = [list(map(int, input().split())) for i in range(n)]
+time_info = [list(map(int, input().split())) for _ in range(n)]
+sorted_time = sorted(time_info, key=lambda data: (data[1], data[0]))
+end = 0
+cnt = 0
 
+for i in range(0, n):
+    if sorted_time[i][0] >= end:
+        end = sorted_time[i][1]
+        cnt += 1
+
+print(cnt)
