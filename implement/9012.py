14de3f2317c54e424b7846cc1d219113aa1b7b5d
sayyyho <323psh@naver.com>
2023-02-04 02:36:26 +0900

    solved : tag - implement(9012)

---
+++ implement/9012.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for _ in range(n):
+    datas = list(input())
+    cnt = 0
+    for data in datas:
+        if data == '(': cnt+=1
+        if data == ')': cnt-=1
+        if cnt < 0: break
+    if cnt == 0: print('YES')
+    else: print("NO")
+
