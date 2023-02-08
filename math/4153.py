09cc00cbd6e93176d5888fec44e860136f5a1746
sayyyho <323psh@naver.com>
2023-02-08 15:04:51 +0900

    solved : tag - math(4153)

---
+++ math/4153.py
@@ -0,0 +1,6 @@
+while True:
+    length = list(map(int, input().split()))
+    if length[0] == 0 and length[1] == 0 and length[2] == 0: break
+    length.sort()
+    if length[0]**2 + length[1]**2 == length[2]**2: print("right")
+    else: print("wrong")
