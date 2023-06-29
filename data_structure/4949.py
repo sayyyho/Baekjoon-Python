f23a48941997b331ef6aeeb460d9d0697414cda9
sayyyho <323psh@naver.com>
2023-06-29 23:20:06 +0900

    230629 : solve 4949

--- data_structure/4949.py
+++ data_structure/4949.py
@@ -1,1 +1,22 @@
-# re
+while True:
+    stack = list()
+    flag = 0
+    string = input()
+    if string == ".": break
+    for c in string:
+        if c == "(": stack.append(c)
+        elif c == "[": stack.append(c)
+        elif c ==")":
+            if stack and stack[-1] == "(":
+                stack.pop()
+            else:
+                flag = 1
+                break
+        elif c =="]":
+            if stack and stack[-1] == "[":
+                stack.pop()
+            else:
+                flag = 1
+                break    
+    if flag == 1 or stack: print("no")
+    else: print("yes")       
