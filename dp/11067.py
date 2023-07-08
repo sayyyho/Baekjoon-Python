e3e09f392c7044a2de663127c9ea7576b3f18d0b
sayyyho <323psh@naver.com>
2023-07-09 01:35:16 +0900

    230708 solved 11067[dp]

---
+++ dp/11067.py
@@ -0,0 +1,9 @@
+n = int(input())
+# 현재 줄의 오르막수
+dp = [[1 for _ in range(10)] for _ in range(n+1)]
+
+for i in range(2,n+1):
+    for j in range(9,0,-1):
+        for k in range(1,j+1):
+            dp[i][j] += dp[i-1][k] % 10007
+print(sum(dp[n]) % 10007)
