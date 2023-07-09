d453995b5ea312466320cd7078ea154668963c15
sayyyho <323psh@naver.com>
2023-07-09 19:03:38 +0900

    230709 solved 9465[dp]

---
+++ dp/9465.py
@@ -0,0 +1,18 @@
+test_case = int(input())
+
+for _ in range(test_case):
+    n = int(input())
+    data = list()
+    for _ in range(2): data.append(list(map(int, input().split())))
+    if n == 1: print(max(data[0][0], data[1][0]))
+    elif n == 2: print(max(data[0][0] + data[1][1], data[1][0] + data[0][1]))
+    else:
+        dp = [[0 for _ in range(n)] for _ in range(2)]
+        dp[0][0] = data[0][0]
+        dp[1][0] = data[1][0]
+        dp[0][1] = dp[1][0] + data[0][1]
+        dp[1][1] = dp[0][0] + data[1][1] 
+        for i in range(2,n):
+            dp[0][i] = data[0][i] + max(dp[1][i-1], dp[1][i-2]) 
+            dp[1][i] = data[1][i] + max(dp[0][i-1], dp[0][i-2])
+        print(max(dp[0][-1], dp[1][-1]))
