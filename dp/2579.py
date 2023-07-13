17f814eae8c65ce0c7e0d490baa9fd494b195004
sayyyho <323psh@naver.com>
2023-07-13 13:59:39 +0900

    230713 solved 2579[dp]

--- dp/2579.py
+++ dp/2579.py
@@ -1,1 +1,13 @@
-# 다시
+n = int(input())
+data = [0]
+for _ in range(n):
+    data.append(int(input()))
+if n == 1: print(data[1])
+elif n == 2: print(data[1] + data[2])
+else:
+    dp = [0 for _ in range(n+1)]
+    dp[1] = data[1]
+    dp[2] = data[1] + data[2]
+    for i in range(3,n+1):
+        dp[i] = max(dp[i-2] + data[i], dp[i-3] + data[i-1] + data[i])
+    print(dp[-1])
