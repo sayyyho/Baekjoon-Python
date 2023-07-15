d0579db40de001c658be9661171516e5b7a58ab1
sayyyho <323psh@naver.com>
2023-07-15 21:37:25 +0900

    230715 solved 11053[dp]

--- dp/11053.py
+++ dp/11053.py
@@ -1,28 +1,8 @@
-# n = int(input())
-# data = list(map(int, input().split()))
-# dp = [1 for _ in range(n)]
+n = int(input())
+data = list(map(int, input().split()))
+dp = [1 for _ in range(n)]
 
-# if n == 1: print(1)
-# elif n == 2: print(2) if data[0] < data[1] else print(1)
-# else:
-#     memo = list()
-#     memo.append(data[0])
-#     for i in range(1, n):
-#         if memo[-1] < data[i]: 
-#             memo.append(data[i])
-#             dp[i] = dp[i-1] + 1
-#         else:
-#             if dp[i-1] <= dp[i] and memo[0] > data[i]:
-#                 memo = [data[i]]
-#                 dp[i] = 1
-#             elif n - i >= n//2 and memo[-1] > data[i] and memo[-2] != data[i]:
-#                 while (data[i] < memo[-1]): 
-#                     memo.pop()
-#                     if len(memo) == 0: break
-#                 memo.append(data[i])
-#                 dp[i] = len(memo) 
-#             else:
-#                 dp[i] = dp[i-1] 
-#         print(memo)
-#     print(dp[n-1])
-# 다시 !!!!
+for i in range(n):
+    for j in range(i):
+        if data[i] > data[j]: dp[i] = max(dp[j] + 1, dp[i])
+print(max(dp))
