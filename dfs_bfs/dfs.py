f197622201c088703782aea8bf1f96efd3a0be97
sayyyho <323psh@naver.com>
2023-08-04 18:16:00 +0900

    Docs: up-to-date

--- dfs_bfs/dfs.py
+++
@@ -1,9 +0,0 @@
-def dfs(visited, now, data):
-    if (visited[now] == True):
-        return
-    visited[now] = True
-    print(now, end=" ")
-    for search in data[now]:
-        if not visited[search]:
-            dfs(visited, search, data)
-    return
--- sort/10825.py
+++ sort/10825.py
@@ -10,3 +10,16 @@ for i in range(n):
     data[i][3] = int(data[i][3])
 data.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
 for value in data: print(value[0])
+
+
+# import sys; input = sys.stdin.readline
+
+# N = int(input())
+# score = []
+# for _ in range(N):
+#     n, k, e, m = input().split()
+#     score.append((-int(k), int(e), -int(m), n))
+# score.sort()
+
+# for i in range(N):
+#     print(score[i][-1])
